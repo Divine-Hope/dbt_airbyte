563daa65b17650daacb83d63050d94ecdd0caea4
Divine <emmanueldivinehope@gmail.com>
2023-02-05 21:17:15 +0100

    correction of table name

--- models/cases.sql
+++ models/cases.sql
@@ -1,1 +1,1 @@
-select * from `modern-data-stack-demo.demo_dataset.covid_data` where new_recovered > new_confirmed
+select * from `decent-slice-363510.data_analysis.covid_data` where new_recovered > new_confirmed
